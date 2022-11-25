06a1c373998eb41a25fb54dff5673ca1fd05a4e0
yhjang <siabard@gmail.com>
2022-11-25 23:04:40 +0900

    한글출력 기능 추가

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use sdl2::TimerSubsystem;
+use hangul_jaso::*;
+use jaso_sdl2::*;
+use sdl2::image::*;
+use sdl2::render::Texture;
+use std::collections::HashMap;
 
 fn main() -> Result<(), String> {
     let context = sdl2::init().unwrap();
@@ -17,6 +21,18 @@ fn main() -> Result<(), String> {
     let texture_creator = canvas.texture_creator();
     let mut event_pump = context.event_pump().unwrap();
 
+    let mut bitmap_fonts: HashMap<Languages, &Texture> = HashMap::new();
+    let kor_bitmap_fonts = texture_creator
+        .load_texture("assets/bitmap_fonts/hangul-dkby-dinaru-2.png")
+        .unwrap();
+    let eng_bitmap_fonts = texture_creator
+        .load_texture("assets/bitmap_fonts/ascii-plain.png")
+        .unwrap();
+
+    bitmap_fonts.insert(Languages::Hangul, &kor_bitmap_fonts);
+    bitmap_fonts.insert(Languages::HangulJamo, &kor_bitmap_fonts);
+    bitmap_fonts.insert(Languages::Ascii, &eng_bitmap_fonts);
+
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -26,7 +42,27 @@ fn main() -> Result<(), String> {
         }
 
         canvas.clear();
+        print_string(
+            &mut canvas,
+            &bitmap_fonts,
+            0,
+            0,
+            220,
+            300,
+            &"다람쥐쳇바퀴돌았따 가나다 01234 ABCD()-+_!@#$%^&*()".to_string(),
+        );
+
+        print_string(
+            &mut canvas,
+            &bitmap_fonts,
+            0,
+            120,
+            150,
+            90,
+            &"동해물과 백두산이".to_string(),
+        );
         canvas.present();
+
         ::std::thread::sleep(std::time::Duration::new(0, 1_000_000_000u32 / 60));
     }
 
